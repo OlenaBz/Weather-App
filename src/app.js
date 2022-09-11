f20842c38b630a5cdc8ec0a1079c5bb59aa69734
Olena <olkvitka@ukr.net>
2022-09-11 23:19:10 +0300

    Started JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#nowTemperature");
+  temperatureElement.innerHTML = `${temperature}`;
+}
+let apiKey = "ebd45fdbc0cbc42c7b17f64eef930b5d";
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(`${apiUrl}`).then(showTemperature);
