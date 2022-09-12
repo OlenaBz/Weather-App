8263db41cae33c6e4e97907454205e48ce98f340
Olena <olkvitka@ukr.net>
2022-09-12 22:58:21 +0300

    Show temperature in city added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#nowTemperature");
-  temperatureElement.innerHTML = `${temperature}`;
+function nowTemperature(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#cityInput");
+  let city = document.querySelector("#city");
+  city.innerHTML = `${cityInput.value}`;
+
+  let apiKey = "a2a8582acba62566c4fd2c9a487348b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}`;
+  function showTemperature(response) {
+    let temperature = Math.round(response.data.main.temp);
+    let temperatureElement = document.querySelector("#nowTemperature");
+    temperatureElement.innerHTML = `${temperature}`;
+  }
+
+  axios.get(`${apiUrl}&appid=${apiKey}&units=metric`).then(showTemperature);
 }
-let apiKey = "ebd45fdbc0cbc42c7b17f64eef930b5d";
-let city = "Kyiv";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(`${apiUrl}`).then(showTemperature);
+let buttonSearch = document.querySelector("#buttonSearch");
+buttonSearch.addEventListener("click", nowTemperature);
+let form = document.querySelector("#form");
+form.addEventListener("submit", nowTemperature);
