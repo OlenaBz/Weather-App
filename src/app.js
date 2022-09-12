9fa5f9e0784e5707ccd1f16d8675a3138a944291
Olena <olkvitka@ukr.net>
2022-09-12 23:32:24 +0300

    Date time js added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,46 @@
+let now = new Date();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+let date = now.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let todayDate = document.querySelector("#todayTime");
+todayDate.innerHTML = ` ${month} ${date},${day} ${hours}:${minutes}`;
+
+function showDate() {
+  let date = `${day} ${now.getHours()}`;
+}
+
 function nowTemperature(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#cityInput");
@@ -10,11 +53,16 @@ function nowTemperature(event) {
     let temperature = Math.round(response.data.main.temp);
     let temperatureElement = document.querySelector("#nowTemperature");
     temperatureElement.innerHTML = `${temperature}`;
+
+    let humidity = response.data.main.humidity;
+    let humidityElement = document.querySelector("#nowHumidity");
+    let wind = response.data.wind.speed;
+    humidityElement.innerHTML = `Humidity: ${humidity}%, wind: 2 m/s`;
   }
 
   axios.get(`${apiUrl}&appid=${apiKey}&units=metric`).then(showTemperature);
 }
 let buttonSearch = document.querySelector("#buttonSearch");
-buttonSearch.addEventListener("click", nowTemperature);
+buttonSearch.addEventListener("click", nowTemperature, showDate);
 let form = document.querySelector("#form");
-form.addEventListener("submit", nowTemperature);
+form.addEventListener("submit", nowTemperature, showDate);
